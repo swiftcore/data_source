c0979676396436c9ee7da2d372ca024235922c47
swiftcore <edvard.wong@qq.com>
2015-07-17 18:58:20 +0800

    rewrite fetch callback with closure

--- client/fetch.py
+++ client/fetch.py
@@ -8,48 +8,47 @@ import os
 import sys
 
 # 如下地址不要改为localhost，否则会因为DNS解析导致RPC调用极其缓慢。
-proxy=xmlrpclib.ServerProxy('http://127.0.0.1:10000')
 
+def context(config,dataname):
+	section='server'
+	host = config.get(section,'host')
+	port = config.get(section,'port')
+	proxy = xmlrpclib.ServerProxy('http://%s:%s' % (host,port))
 
-def display_info(row):
-	print row,',',
+	def submit_record(stk,row):
+		proxy.add_bar(stk,row)
 
-def display_time(tm):
-	print tm,
+	def tick(indata):
+		if indata.ErrorCode != 0:
+			print 'ErrorCode %s' % indata.ErrorCode
+			return
 
-def submit_record(stk,row):
-	global proxy
-	proxy.add_bar(stk,row)
+		for ix in xrange(len(indata.Codes)):
+			row = []
+			tm = indata.Times[0].strftime('%H:%M:%S')
+			row.append(tm)
+			for k in xrange(len(indata.Fields)):
+				if indata.Fields[k] == u'RT_UPWARD_VOL':
+					row.append('B')
+					row.append(indata.Data[k][ix])
+				elif indata.Fields[k] == u'RT_DOWNWARD_VOL':
+					row.append('S')
+					row.append(indata.Data[k][ix])
+				else:
+					row.append(indata.Data[k][ix])
 
-def democb(indata):
-	if indata.ErrorCode != 0:
-		print 'ErrorCode %s' % indata.ErrorCode
-		return
+			stk = indata.Codes[ix]
+			submit_record(stk,row)
 
-	#f = display_info
-	f = submit_record
+	calls = {}
+	calls['tick'] = tick
 
-	for ix in xrange(len(indata.Codes)):
-		row = []
-		tm = indata.Times[0].strftime('%H:%M:%S')
-		row.append(tm)
-		for k in xrange(len(indata.Fields)):
-			if indata.Fields[k] == u'RT_UPWARD_VOL':
-				row.append('B')
-				row.append(indata.Data[k][ix])
-			elif indata.Fields[k] == u'RT_DOWNWARD_VOL':
-				row.append('S')
-				row.append(indata.Data[k][ix])
-			else:
-				row.append(indata.Data[k][ix])
-
-		stk = indata.Codes[ix]
-		f(stk,row)
+	return calls[dataname] if dataname in calls else None
 
 def get_fetch_config(conf_filename):
 	section = 'fetch'
 	conf = ConfigParser.ConfigParser()
-	conf.readfp(open('connection.cfg'))
+	conf.readfp(open(conf_filename))
 	codes_str = conf.get(section,'codes')
 	fields_str = conf.get(section,'fields')
 	codes = codes_str.split(',')
@@ -60,7 +59,7 @@ def fetch(conf_file):
 	config = get_fetch_config(conf_file)
 	# rt_time,rt_low,rt_last,rt_last,rt_last_vol,rt_upward_vol,rt_downward_vol
 	#data = w.wsq('000001.SZ','rt_last,rt_last_vol,rt_upward_vol,rt_downward_vol',func=democb)
-	data = w.wsq(config['codes'],config['fields'],func=democb)
+	data = w.wsq(config['codes'],config['fields'],func=context(config,'tick'))
 	return data
 
 if __name__ == '__main__':
